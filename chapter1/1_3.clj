0bcb92a398c0a041b6be100938ff89f9d8b14508
FND <fnd@localhost.localdomain>
2012-07-09 18:27:40 +0200

    exercise 1.3
    this took me embarrassingly long and I eventually had to resort to
    cheating (http://sicp.org.ua/sicp/Exercise1-3) to get the details right
    
    clearly my brain's no good for this kind of algorithm

---
+++ chapter1/1_3.clj
@@ -0,0 +1,14 @@
+(defn sum-of-largest-squares [a b c]
+
+    (def max1 (if (> a b) a b))
+    (def min (if (< a b) a b))
+    (def max2 (if (> min c) min c))
+
+    (+ (* max1 max1) (* max2 max2)))
+
+(prn (sum-of-largest-squares 5 7 11))
+(prn (sum-of-largest-squares 5 11 7))
+(prn (sum-of-largest-squares 7 5 11))
+(prn (sum-of-largest-squares 7 11 5))
+(prn (sum-of-largest-squares 11 5 7))
+(prn (sum-of-largest-squares 11 7 5))
