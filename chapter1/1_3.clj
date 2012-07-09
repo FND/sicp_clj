30a37a85be8b4fc32198651a5a3f96a19a0cd3dc
FND <fnd@localhost.localdomain>
2012-07-09 19:11:57 +0200

    fixed variable scope
    `let` provides local variables, thus avoiding name clash with
    `clojure.core/min`
    
    I really have no idea how this stuff might be indented properly...

--- chapter1/1_3.clj
+++ chapter1/1_3.clj
@@ -1,10 +1,10 @@
 (defn sum-largest-squares [a b c]
 
-    (def max1 (if (> a b) a b))
-    (def min (if (< a b) a b))
-    (def max2 (if (> min c) min c))
+    (let [max1 (if (> a b) a b)
+            min (if (< a b) a b)
+            max2 (if (> min c) min c)]
 
-    (+ (* max1 max1) (* max2 max2)))
+        (+ (* max1 max1) (* max2 max2))))
 
 (prn (sum-largest-squares 5 7 11))
 (prn (sum-largest-squares 5 11 7))
